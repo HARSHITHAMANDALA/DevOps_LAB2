b1cfcd6836940cdaa79a3c27f5e55af13e32df37
Manogar C <manogarbtech@gmail.com>
2026-07-14 04:46:22 +0530

    Search character function has been created

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
+def search_character(text):
+  temp=char(input("Enter the character you want to search:"))
+  for i in text:
+    if i==temp:
+      return True
+  return False
+			
+
 while True:
   print("String Operations")
   print("1.Convert case")
@@ -5,16 +13,22 @@ while True:
   print("3.Search Character")
   print("4.Replace Character")
 
+text=str(input("Enter your string:"))
 choice=int(input("Enter choice:"))
 
 if choice==1:
-  convert_case()
+  convert_case(text)
 elif choice==2:
-  reverse_string()
+  reverse_string(text)
 elif choice==3:
-  search_character()
+  result=search_character(text)
+  if result:
+    print("Found the character")
+  else:
+    print("Not found") 
 elif choice==4:
-  replace_character()
+  replace_character(text)
 else :
   print("Invalid choice")
   break
+
