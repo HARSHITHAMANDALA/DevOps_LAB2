798e507545e62608fd513921ab89eae87e3a291d
neeharika-kuralla <152521119+neeharika-kuralla@users.noreply.github.com>
2026-07-14 09:49:32 +0530

    test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+while True:
+  print("String Operations")
+  print("1.Convert case")
+  print("2.Reverse String")
+  print("3.Search Character")
+  print("4.Replace Character")
+
+choice=int(input("Enter choice:"))
+
+if choice==1:
+  convert_case()
+elif choice==2:
+  reverse_string()
+elif choice==3:
+  search_character()
+else :
+  print("Invalid choice")
+  break
